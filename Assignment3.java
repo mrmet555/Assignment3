0b7adc2aeb0edd6a0e56eaa92348c61857deb946
Daniel <mrmet555@gmail.com>
2015-10-08 14:10:14 -0400

    assignment 3

--- Assignment3.java
+++ Assignment3.java
@@ -43,7 +43,7 @@ public class Assignment3 {
 							JOptionPane.showMessageDialog(null, "Please select your account type again.");
 							input = (AccType) JOptionPane.showInputDialog(null, "Please select your account type", null, JOptionPane.QUESTION_MESSAGE, null, account, account[0]);
 						}	
-					}else if (!user.equals(coruser[userindex]) && !pass.equals(corpass[passindex]) && userindex<2 && passindex<2){
+					}if ((!user.equals(coruser[userindex]) || !pass.equals(corpass[passindex])) && userindex<2 && passindex<2){
 						userindex++;
 						passindex++;
 					}else {
@@ -51,12 +51,40 @@ public class Assignment3 {
 						user = JOptionPane.showInputDialog("Please enter your username");
 						pass = JOptionPane.showInputDialog("Please enter your password");
 						tries++;
+						if (user.equals(coruser[userindex]) && pass.equals(corpass[passindex])){
+							String Acc = input.toString();
+							switch(Acc){
+							case "Administrator":
+								if (userindex == 0){
+									JOptionPane.showMessageDialog(null, "Welcome Admin! You can update and read files.");
+									System.exit(0);
+								}
+							case "Staff":
+								if (userindex == 1){
+									JOptionPane.showMessageDialog(null, "Welcome Student! You can only read files.");
+									System.exit(0);
+								}
+							case "Student":
+								if (userindex == 2){
+									JOptionPane.showMessageDialog(null, "Welcome Staff! You can update, read, add, delete file.");
+									System.exit(0);
+								}
+							default: 
+								JOptionPane.showMessageDialog(null, "Please select your account type again.");
+								input = (AccType) JOptionPane.showInputDialog(null, "Please select your account type", null, JOptionPane.QUESTION_MESSAGE, null, account, account[0]);
+							}	
+						}if ((!user.equals(coruser[userindex]) || !pass.equals(corpass[passindex])) && userindex<2 && passindex<2){
+							userindex++;
+							passindex++;
 						//increment tries number each time the username or password is in correct
 					}
-				
+					}
 			}
+				
+			
 		//end loop and display exit message
 		 JOptionPane.showMessageDialog(null, "Contact Administrator!");
 		 System.exit(0);
-}
+
+	}
 }
